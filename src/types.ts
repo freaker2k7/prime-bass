ce472d3179204a5dc56c4c0b46b0c5b1d0d3beda
Evgeny Kolyakov <thenetfreaker@gmail.com>
2026-07-20 10:22:34 +0300

    Updated Packages

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,6 @@
 export type FilterRollOff = -12 | -24 | -48 | -96;
 
 export type FilterType =
-	| 'lowpass'
-	| 'highpass'
-	| 'bandpass'
-	| 'notch'
-	| 'allpass'
-	| 'peaking'
-	| 'lowshelf'
-	| 'highshelf';
+	'lowpass' | 'highpass' | 'bandpass' | 'notch' | 'allpass' | 'peaking' | 'lowshelf' | 'highshelf';
 
 export type OscillatorType = 'sine' | 'square' | 'triangle' | 'sawtooth';
